aec8ded9468bb466ef5610799b048287c2beed0f
Christopher Abad <aempirei@gmail.com>
2011-02-28 10:43:29 -0800

    i

---
+++ types.cc
@@ -0,0 +1,19 @@
+#include <string.h>
+
+#include "tchatd.hh"
+#include "types.hh"
+
+bool strcase_compar::operator()(const char * str1, const char *str2) {
+	return strcasecmp(str1, str2);
+}
+
+node::node(unsigned int id, unsigned int distance) : id(id), distance(distance) {
+}
+
+user *node::get_user() {
+	return state::users_by_id[id];
+}
+
+int node::get_fd() {
+	return state::fd_by_id[id];
+}
